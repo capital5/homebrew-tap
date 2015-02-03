fb2eead0bcd752166d237bd0f5d23d32ad7fe42f
Brian Johnson <github@brianjohnson.cc>
2015-02-03 13:04:20 -0700

    fix sqoop dependencies

--- sqoop-postgres.rb
+++ sqoop-postgres.rb
@@ -5,8 +5,8 @@ class SqoopPostgres < Formula
   url 'http://korrelate-public-repo.s3.amazonaws.com/hadoop/sqoop/sqoop-1.4.4.bin__hadoop-2.0.4-alpha.tar.gz'
   version '1.4.4'
 
-  depends_on 'hadoop'
-  depends_on 'hbase'
+  depends_on 'hadoop-2.5.2'
+  depends_on 'hbase-0.98.8'
   depends_on 'hive'
   depends_on 'zookeeper'
 

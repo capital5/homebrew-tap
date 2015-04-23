6a32c72c54d42859bfd1d80a64f0618fa2eb950f
Billy Watson <williamrwatson@gmail.com>
2015-04-23 13:51:40 -0400

    set the hadoop classpath, adding hadoop tools so s3 URLs work

--- hadoop-2.6.0.rb
+++ hadoop-2.6.0.rb
@@ -5,6 +5,10 @@ class Hadoop260 < Formula
   url 'http://s3.amazonaws.com/korrelate-public-repo/hadoop/hadoop/hadoop-2.6.0.tar.gz'
   sha1 '5b5fb72445d2e964acaa62c60307168c009d57c5'
 
+  # this patch is needed for hadoop 2.6.0 and following to work with s3 URLs. It adds
+  # the tools directory to the classpath which used to be a part of the classpath by default.
+  patch :DATA
+
   def install
     rm_f Dir["bin/*.cmd", "sbin/*.cmd", "libexec/*.cmd", "etc/hadoop/*.cmd"]
     libexec.install %w[bin sbin libexec share etc]
@@ -31,6 +35,20 @@ class Hadoop260 < Formula
       #{libexec}/etc/hadoop/yarn-env.sh
     $JAVA_HOME has been set to be the output of:
       /usr/libexec/java_home
+    You must set $HADOOP_HOME in your bash profile for this shiz to work 
     EOS
   end
 end
+
+# diff created by using the instructions here:
+# https://github.com/Homebrew/homebrew/blob/master/share/doc/homebrew/Formula-Cookbook.md#creating-the-diff
+__END__
+diff --git a/etc/hadoop/hadoop-env.sh b/etc/hadoop/hadoop-env.sh
+index f60b65a..0c3395a 100644
+--- a/etc/hadoop/hadoop-env.sh
++++ b/etc/hadoop/hadoop-env.sh
+@@ -96,3 +96,4 @@ export HADOOP_SECURE_DN_PID_DIR=${HADOOP_PID_DIR}
+ 
+ # A string representing this instance of hadoop. $USER by default.
+ export HADOOP_IDENT_STRING=$USER
++export HADOOP_CLASSPATH="$HADOOP_HOME/libexec/share/hadoop/tools/lib/*:$HADOOP_CLASSPATH"
